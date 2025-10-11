9842152708edd21e8ee1b47c38f3f43d32f3c97d
SmbUdk <rahmanyazwork@gmail.com>
2025-10-11 19:20:27 +0300

    checkbutton

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import React,{useState} from 'react';
 // App.js
+import './App.css';
+
 
 export default function MyApp() {
   const [task, setTask] = useState('');
   const  [tasks, setTasks] = useState([]);
   const [clicked,setClicked] = useState([]);
+  
   const handleAdd = () => {
     if (task.trim()){
       setTasks([...tasks,task]);
       setClicked([...clicked,false]);
       setTask('');
     }};
+  const handleDelete = ()=> {
+   const chooesen = Array(tasks.length).fill(false);
+   return (
+    <div>
+      clicked = chooesen ;
+      {tasks.map((t,index)=>
+        <li key={index}> <button onClick={() => handleCheck(index)} id="checkbutton" >  {clicked[index] ? '✅':'🔲'}</button>
+        &nbsp;{t} 
+        </li>)}
+    </div>
+   )
+  }
   const handleCheck = (index)=>{
-    const updated = [...clicked];
-    setClicked(updated);  
+    setClicked(clicked => {
+      const updated = [...clicked];
+      updated[index]=!updated[index];
+      return updated;
+    });
   };
   return (
     <div>
       <h1>To do list</h1>
-      <input type="text" value={task} onChange={(e) => setTask(e.target.value)}  placeholder='New task' / >
-      <button onClick={handleAdd} >Add</button>
-      <ul>
-        {tasks.map((t,index)=><li key={index}>{t} 
-        <button onClick={() => handleCheck(index)}>  {clicked[index] ? '✅' : '🔲'}</button>
+      <input type="text" value={task} onChange={(e) => setTask(e.target.value)}  placeholder='New task' / >&nbsp;
+      <button onClick={handleAdd} >Add</button>  &nbsp;&nbsp;&nbsp;  <button onClick={handleDelete}>Delete</button>
+      
+        {tasks.map((t,index)=>
+        <li key={index}> <button onClick={() => handleCheck(index)} id="checkbutton" >  {clicked[index] ? '✅':'🔲'}</button>
+        &nbsp;{t} 
         </li>)}
-      </ul>
+     
     </div>
   );
 }
+ 
